2dc9370f33ee5ca56d7c87cfcff009dd7805620f
itsfatduck <124025976+itsfatduck@users.noreply.github.com>
2026-02-27 13:41:51 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/layout.tsx
@@ -0,0 +1,67 @@
+import {Footer, Layout, Navbar} from 'nextra-theme-docs'
+import {Head} from 'nextra/components'
+import {getPageMap} from 'nextra/page-map'
+import './globals.css'
+import {Metadata} from "next";
+import {NextraSearchDialog} from "@/components/nextra-search-dialog";
+import {getPagesFromPageMap} from "@/lib/getPagesFromPageMap";
+
+export const metadata: Metadata = {
+    // Define your metadata here
+    // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
+}
+
+// const banner = <Banner storageKey="some-key">This template was created with 🩸 and 💦 by <Link href="https://github.com/phucbm">PHUCBM</Link> 🐧</Banner>
+const navbar = (
+    <Navbar
+        projectLink="https://github.com/phucbm/nextra-docs-starter"
+        logo={<img src="/images/general/logo.svg" alt="Logo" width={100} height={20}/>}
+        // ... Your additional navbar options
+    />
+)
+const footer = <Footer>MIT {new Date().getFullYear()} © Nextra.</Footer>
+
+export default async function RootLayout({children}) {
+    const pageMap = await getPageMap();
+    const pages = await getPagesFromPageMap({
+        pageMapArray: pageMap,
+        // modify page data if needed
+        // filterItem: async (item) => {
+        //     return {
+        //         ...item,
+        //     };
+        // }
+    });
+
+
+    return (
+        <html
+            // Not required, but good for SEO
+            lang="en"
+            // Required to be set
+            dir="ltr"
+            // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
+            suppressHydrationWarning
+        >
+        <Head
+            // ... Your additional head options
+        >
+            <link rel="shortcut icon" href="/images/general/icon.svg"/>
+            {/* Your additional tags should be passed as `children` of `<Head>` element */}
+        </Head>
+        <body>
+        <Layout
+            // banner={banner}
+            navbar={navbar}
+            pageMap={pageMap}
+            docsRepositoryBase="https://github.com/phucbm/nextra-docs-starter/tree/main"
+            footer={footer}
+            search={<NextraSearchDialog pages={pages}/>}
+            // ... Your additional layout options
+        >
+            {children}
+        </Layout>
+        </body>
+        </html>
+    )
+}
